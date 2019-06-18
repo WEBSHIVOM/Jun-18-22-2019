0ebf3a100bfd04d7502cf197c3482ff8e40ebde8
prabhu-durasoft <iamprabhu@yahoo.com>
2019-06-18 15:50:56 +0530

    Day01

---
+++ Day01/src/com/lab04/Input.java
@@ -0,0 +1,25 @@
+package com.lab04;
+
+public class Input {
+	private int a;
+	private int b;
+	private int result;
+	
+	public Input(int a, int b, int result) {
+		this.a = a;
+		this.b = b;
+		this.result = result;
+	}
+
+	public int getA() {
+		return a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public int getResult() {
+		return result;
+	}
+}
---
+++ Day01/src/com/lab04/Main.java
@@ -0,0 +1,27 @@
+package com.lab04;
+
+import java.util.function.Function;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Function<Input, Input> aSquare = 
+				(ip) -> new Input(ip.getA(), ip.getB(), ip.getResult() + (ip.getA() * ip.getA()));
+		
+		Function<Input, Input> bSquare = 
+				(ip) -> new Input(ip.getA(), ip.getB(), ip.getResult() + (ip.getB() * ip.getB()));
+		
+		Function<Input, Input> _2ab = 
+				(ip) -> new Input(ip.getA(), ip.getB(), ip.getResult() + (ip.getA() * ip.getB() * 2));
+		
+		Input formula = 
+				aSquare
+					.andThen(bSquare)
+					.andThen(_2ab)
+					.apply(new Input(10, 12, 0));
+		
+		System.out.println(formula.getResult());
+	}
+
+}
