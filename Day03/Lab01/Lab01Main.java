95d11c17803774b342c523a8dd1b38c66aff70ae
prabhu-durasoft <iamprabhu@yahoo.com>
2019-06-20 13:34:07 +0530

    Day03

---
+++ Day03/Lab01/Lab01Main.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lab01Main {
+
+	public static void main(String[] args) {
+		//Load the data of all the movies
+		List<Movie> movies = Data.getMovies();
+		
+		//ToDo
+		
+		//Print the name of all the movies that Amitabh has acted
+		
+		//Print the nationality of all actors who have acted in movies in between 2000 - 2010
+		
+		//Print the name of all the movies grouped by actor and supporting actors
+	}
+}
